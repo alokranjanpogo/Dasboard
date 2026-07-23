a5f10cd95868262cbd57ffb44dbdd27644a4292d
alokranjanpogo <alokranjan18april@gmail.com>
2026-07-23 12:32:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import plotly.express as px
+import plotly.graph_objects as go
 from utils.loader import *
 
 st.set_page_config(
