628a84bb44c6ef86c560d46043062b6bd608aaf0
alokranjanpogo <alokranjan18april@gmail.com>
2026-07-23 12:18:46 +0530

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -5,24 +5,27 @@ stock_file = "chemical_stock.xlsx"
 
 
 def load_consumption():
-    sheets = pd.read_excel(
+    return pd.read_excel(
         consumption_file,
         sheet_name=None,
         engine="openpyxl"
     )
-    return sheets
 
 
 def load_stock():
-    sheets = pd.read_excel(
+    return pd.read_excel(
         stock_file,
         sheet_name=None,
         engine="openpyxl"
     )
-    return sheets
 
 
-def get_sheet_names():
-    consumption = load_consumption()
-    stock = load_stock()
-    return list(consumption.keys()), list(stock.keys())
+def get_latest_stock():
+
+    sheets = load_stock()
+
+    latest_sheet = list(sheets.keys())[-1]
+
+    df = sheets[latest_sheet]
+
+    return latest_sheet, df
