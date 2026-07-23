606c458ba3b31d84f9ce1ef808ef8d102ffb3d8c
alokranjanpogo <alokranjan18april@gmail.com>
2026-07-23 12:14:10 +0530

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,35 +1,28 @@
 import pandas as pd
 
-consumption_file="data/chemical_consumption.xlsx"
-stock_file="data/chemical_stock.xlsx"
+consumption_file = "chemical_consumption.xlsx"
+stock_file = "chemical_stock.xlsx"
 
 
 def load_consumption():
-
-    sheets=pd.read_excel(
+    sheets = pd.read_excel(
         consumption_file,
         sheet_name=None,
         engine="openpyxl"
     )
-
     return sheets
 
 
 def load_stock():
-
-    sheets=pd.read_excel(
+    sheets = pd.read_excel(
         stock_file,
         sheet_name=None,
         engine="openpyxl"
     )
-
     return sheets
 
 
 def get_sheet_names():
-
-    consumption=load_consumption()
-
-    stock=load_stock()
-
-    return list(consumption.keys()),list(stock.keys())
+    consumption = load_consumption()
+    stock = load_stock()
+    return list(consumption.keys()), list(stock.keys())
